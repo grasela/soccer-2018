68f89b99ee4ce9040ca8f0f88cf3cafe093d7365
Eryk Grasela <grasela.eryk@gmail.com>
2018-06-19 11:12:59 +1000

    fix css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
-import Match from './Components/Match'
-
-
+import "./App.css";
+import Match from "./Components/Match";
 
 class App extends Component {
-
   state = {
     message: "Hello World",
     matches: null,
@@ -15,94 +12,89 @@ class App extends Component {
     },
     teams: null,
     stadium: null,
-    filterStr: ''
-    
+    filterStr: ""
+  };
 
-  }
-  
   componentDidMount() {
-    this.fetchGroupMatches()
-    .catch(err => console.error(err))
+    this.fetchGroupMatches().catch(err => console.error(err));
   }
 
   async fetchGroupMatches() {
-    const url = 'https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json'
-    const response = await fetch(url)
-    const data = await response.json()
-  
+    const url =
+      "https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json";
+    const response = await fetch(url);
+    const data = await response.json();
 
-    let matches = []
-    
+    let matches = [];
+
     for (const group of Object.keys(data.groups)) {
-      const groupField = data.groups[group]
-      const allMatches = data.groups[group].matches
+      const groupField = data.groups[group];
+      const allMatches = data.groups[group].matches;
       allMatches.map(match => {
-        match.group = groupField
-        matches.push(match)
-      })
-  }
-  
-    
-    const teams = data.teams
-    const stadiums = data.stadiums
+        match.group = groupField;
+        matches.push(match);
+      });
+    }
+
+    const teams = data.teams;
+    const stadiums = data.stadiums;
 
     matches.forEach(match => {
-      const homeTeam = match.home_team
-      const awayTeam = match.away_team
-      const matchStadium = match.stadium
+      const homeTeam = match.home_team;
+      const awayTeam = match.away_team;
+      const matchStadium = match.stadium;
 
-      match.home_team = teams.find(team => homeTeam === team.id)
-      match.away_team = teams.find(team => awayTeam === team.id)
-      match.stadium = stadiums.find(stadium => matchStadium === stadium.id )
-      return match
-    })
-
+      match.home_team = teams.find(team => homeTeam === team.id);
+      match.away_team = teams.find(team => awayTeam === team.id);
+      match.stadium = stadiums.find(stadium => matchStadium === stadium.id);
+      return match;
+    });
 
     this.setState({
       matches: matches,
       group: {
-        name: data.groups.a.name,
+        name: data.groups.a.name
       },
       teams: teams
-    })
+    });
   }
 
-  
   render() {
-    const {group, matches} = this.state
-    const { filterStr } = this.state
+    const { group, matches } = this.state;
+    const { filterStr } = this.state;
 
-
-    
-    if(!group.name) {
-      return <div className="App">Loading...</div>
+    if (!group.name) {
+      return <div className="App">Loading...</div>;
     }
-    
+
     const games = matches
-    .filter( match =>  
-      match.home_team.name.toLowerCase().includes(filterStr) || 
-      match.away_team.name.toLowerCase().includes(filterStr) ||
-      match.group.name.toLowerCase().includes(filterStr)
-    )
-    .map(match => {
-      return <Match key="match.id" {...match} />
-        
-      
-    })
+      .filter(
+        match =>
+          match.home_team.name.toLowerCase().includes(filterStr) ||
+          match.away_team.name.toLowerCase().includes(filterStr) ||
+          match.group.name.toLowerCase().includes(filterStr)
+      )
+      .map(match => {
+        return <Match key="match.id" {...match} />;
+      });
 
     return (
       <div className="App">
-      
-      <div className="input">
-      <h1> World Cup 2018 Games </h1>
-      <input type="text" placeholder="search country or type 'group x'" defaultValue='' onChange={e => { this.setState({filterStr: e.target.value.toLowerCase()})} } />
-      </div>
-      <div className="games">
-      {games}
-      </div>
+        <div className="input">
+          <h1> World Cup 2018 Games </h1>
+          <input
+            type="text"
+            placeholder="search country or type 'group x'"
+            defaultValue=""
+            onChange={e => {
+              this.setState({ filterStr: e.target.value.toLowerCase() });
+            }}
+          />
+        </div>
+        <div className="games">{games}</div>
       </div>
     );
   }
 }
 
-export default App
+export default App;
--- src/Components/Match.js
+++ src/Components/Match.js
@@ -1,55 +1,59 @@
-import React from 'react'
-import moment from 'moment'
+import React from "react";
+import moment from "moment";
 
 class Match extends React.Component {
-    
-    render() {
-    
-    let homeResult = this.props.home_result
-    let awayResult = this.props.away_result
-    
+  render() {
+    let homeResult = this.props.home_result;
+    let awayResult = this.props.away_result;
 
-    let date = moment(this.props.date).format('MMMM Do YYYY, h:mm:ss a')
-
-    if (homeResult === 0){
-        homeResult = "0"
+    let date = moment(this.props.date).format("MMMM Do YYYY, h:mm:ss a");
+    
+    if (homeResult === 0) {
+      homeResult = "0";
     }
 
     if (awayResult === 0) {
-        awayResult = "0"
+      awayResult = "0";
     }
-    return <div className="game">
-    <h1>{this.props.group.name}</h1>
-    <h2>{date}</h2>
+    return (
+      <div className="game">
+        <h1>{this.props.group.name}</h1>
+        <h2>{date}</h2>
         <div className="score">
-            <div>AWAY</div>
-            <div className="vs">STADIUM</div>
-            <div>HOME</div> 
+          <div></div>
+          <div className="vs">STADIUM</div>
+          <div></div>
         </div>
 
         <div className="score">
-            <div>{awayResult || "Not yet played "}</div>
-            <div className="vs"><h3>{this.props.stadium.city}</h3></div>
-            <div>{homeResult || "Not yet played"}</div> 
+          <div>{awayResult || "Not yet played "}</div>
+          <div className="vs">
+            <h3>{this.props.stadium.city}</h3>
+          </div>
+          <div>{homeResult || "Not yet played"}</div>
         </div>
-   
 
         <div className="score">
-            <div><strong>{this.props.away_team.name}</strong></div>
-            <div className="vs"> VS </div>
-            <div><strong>{this.props.home_team.name}</strong></div> 
+          <div>
+            <strong>{this.props.away_team.name}</strong>
+          </div>
+          <div className="vs"> VS </div>
+          <div>
+            <strong>{this.props.home_team.name}</strong>
+          </div>
         </div>
-
 
         <div className="score">
-            <div><img src={this.props.away_team.flag} /></div>
-            <div className="vs"></div>
-            <div><img src={this.props.home_team.flag} /></div> 
+          <div>
+            <img src={this.props.away_team.flag} />
+          </div>
+          <div className="vs" />
+          <div>
+            <img src={this.props.home_team.flag} />
+          </div>
         </div>
-
-
-
-    </div>
+      </div>
+    );
+  }
 }
-}
-export default Match
+export default Match;
